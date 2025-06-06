5a38c64cd9cf669b3d2bcfb69946a106fb80ec59
deonopinallae <deonopinallae@yandex.ru>
2025-06-06 14:11:32 +0300

    React Hook Form and Yup

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,146 +1,103 @@
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useRef, useEffect } from 'react'
 import styles from './Form.module.css'
-import { useState, useRef, useEffect } from 'react'
+
+const sendFormData = (formData) => {
+	console.log(formData)
+}
+
+const schema = yup.object().shape({
+	email: yup
+		.string()
+		.trim()
+		.matches(/^\S+@\S+\.\S+$/, 'Введите корректный email.'),
+	password: yup
+		.string()
+		.trim()
+		.min(6, 'Пароль должен быть больше 6 символов.')
+		.max(20, 'Пароль должен быть не больше 20 символов.')
+		.matches(
+			/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]+$/,
+			'Пароль должен состоять только из букв и цифр и иметь хотя бы одну заглавную букву и цифру.',
+		),
+	reenterPassword: yup
+		.string()
+		.trim()
+		.oneOf([yup.ref('password')], 'Пароли не совпадают'),
+})
 
 export const Form = () => {
-	const [value, setValue] = useState({ email: '', password: '', reenterPassword: '' })
-	const [valueError, setValueError] = useState({
-		emailError: null,
-		passwordError: null,
-		reenterPasswordError: null,
-	})
 	const submitBtn = useRef(null)
 
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid, isDirty },
+	} = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+			reenterPassword: '',
+		},
+		resolver: yupResolver(schema),
+		mode: 'onChange',
+	})
+
+
 	useEffect(() => {
-		checkCurrectInput()
-	}, [value])
-
-	const onBlurEmail = ({ target }) => {
-		let error = null
-		if (!/^\w+.@[a-z]+\.[a-z]+$/.test(target.value)) {
-			error = 'Введите корректный email.'
-		}
-		setValueError({ ...valueError, emailError: error })
-	}
-
-	const onChangePassword = ({ target }) => {
-		let error = []
-		setValue({ ...value, password: target.value })
-		if (!/(?=.*[A-Z])(?=.*\d)/.test(target.value)) {
-			error.push('Пароль должен иметь хотя бы одну заглавную букву и цифру.')
-		}
-		if (/[^a-z\dA-Z]/.test(target.value)) {
-			error.push('Пароль должен состоять только из букв и цифр.')
-		}
-		setValueError({ ...valueError, passwordError: error?.join('\n') })
-	}
-
-	const onBlurPassword = ({ target }) => {
-		let error = []
-		if (target.value.length < 6 || target.value.length > 20) {
-			error.push('Пароль должен быть больше 6 символов и не больше 20 символов.')
-		}
-		setValueError({ ...valueError, passwordError: error?.join('\n') })
-	}
-
-	const onChangeReenterPassword = ({ target }) => {
-		setValue({ ...value, reenterPassword: target.value })
-		target.value === value.password
-			? setValueError({ ...valueError, reenterPasswordError: null })
-			: null
-	}
-	const checkCurrectInput = () => {
-		if (
-			!valueError.emailError &&
-			!valueError.passwordError &&
-			!valueError.reenterPasswordError &&
-			value.email !== '' &&
-			value.password !== '' &&
-			value.reenterPassword === value.password
-		) {
+		if (isDirty && isValid) {
 			submitBtn.current.focus()
 		}
-	}
-
-	const onBlurReenterPassword = ({ target }) => {
-		let error = null
-		target.value !== value.password ? (error = 'Не верный пароль.') : null
-		setValueError({ ...valueError, reenterPasswordError: error })
-	}
-
-    const onSubmit = (event) => {
-        event.preventDefault()
-        console.log(value)
-		setValue({ email: '', password: '', reenterPassword: '' })
-    }
+	}, [yup.ref('reenterPassword')])
 
 	return (
-		<form onSubmit={onSubmit} className={styles.form}>
+		<form onSubmit={handleSubmit(sendFormData)} className={styles.form}>
 			<label>
-				{valueError.emailError && (
-					<div className={styles.error}>{valueError.emailError}</div>
+				{errors.email?.message && (
+					<div className={styles.error}>{errors.email?.message}</div>
 				)}
 				<input
-					value={value.email}
-					onChange={({ target }) => setValue({ ...value, email: target.value })}
-					onBlur={onBlurEmail}
+					className={styles.formInput}
 					placeholder="email"
-					className={styles.formInput}
-					name="email"
-					type="email"
-					required
+					name="login"
+					type="text"
+					{...register('email')}
 				/>
 			</label>
 			<label>
-				{valueError.passwordError && (
-					<div className={styles.error}>{valueError.passwordError}</div>
+				{errors.password?.message && (
+					<div className={styles.error}>{errors.password?.message}</div>
 				)}
 				<input
-					value={value.password}
-					onChange={onChangePassword}
-					onBlur={onBlurPassword}
+					className={styles.formInput}
 					placeholder="password"
-					className={styles.formInput}
+					name="password"
 					type="password"
-					required
+					{...register('password')}
 				/>
 			</label>
-
 			<label>
-				{valueError.reenterPasswordError && (
-					<div className={styles.error}>{valueError.reenterPasswordError}</div>
+				{errors.reenterPassword?.message && (
+					<div className={styles.error}>{errors.reenterPassword?.message}</div>
 				)}
 				<input
-					value={value.reenterPassword}
-					onChange={onChangeReenterPassword}
-					onBlur={onBlurReenterPassword}
-					placeholder="password one more time"
 					className={styles.formInput}
+					placeholder="enter password one more time"
+					name="reenterPassword"
 					type="password"
-					required
+					{...register('reenterPassword')}
 				/>
 			</label>
 			<button
-				disabled={
-					valueError.emailError ||
-					valueError.passwordError ||
-					valueError.reenterPasswordError ||
-					value.email === '' ||
-					value.password === '' ||
-					value.reenterPassword === ''
-				}
-				className={styles.formBtn}
 				type="submit"
+				disabled={!isValid}
+				className={styles.formBtn}
 				ref={submitBtn}
 			>
-				Sign up
+				Submit
 			</button>
 		</form>
 	)
 }
-/*
-валидация:
-проверка емаил на @
-пароль больше 6 символов и не больше 20 символов
-пароль только буквы и цифры, минимум одна большая буква и цифра
-повторный пароль такой же как и пароль
-*/
